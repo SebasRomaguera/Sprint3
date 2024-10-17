b12fc473cb34d06ad8cda700e9398dd0f4522c50
SebasRomaguera <sromagueracamps@cifpfbmoll.eu>
2024-10-17 13:59:07 +0200

    Add files via upload

---
+++ PuntosEstudiante.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class PuntosEstudiante {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Número de resgistro:");
+        int numResgistro = sc.nextInt();
+        System.out.println("¿Tiene algun hermano en el centro  (0: no / 1:si)?:");
+
+    }
+}
